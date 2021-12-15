91843ddd9b862f4aa04bd032f579820bbab7f924
antonio <jtlalpa@jobomas.com>
2021-12-14 21:10:00 -0600

    agrego controller shoe y responsive image

---
+++ src/Controller/ShoeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShoeController extends AbstractController
+{
+    #[Route('/shoe/{id}', name: 'shoe')]
+    public function index($id): Response
+    {
+        return $this->render('shoe/index.html.twig', [
+            'idShoe' => $id,
+        ]);
+    }
+}
