a4ffe3d383ef5378f2bfc6dc429fd0b89c92a77b
antonio <jtlalpa@jobomas.com>
2021-12-30 22:43:45 -0600

    consultando en el controller shoes

--- src/Controller/ShoeController.php
+++ src/Controller/ShoeController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Shoes;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShoeController extends AbstractController
 {
     #[Route('/shoe/{id}', name: 'shoe')]
-    public function index($id): Response
+    public function index($id, ManagerRegistry $doctrine): Response
     {
+        $em = $doctrine->getManager();
+        $shoes = $em->getRepository(Shoes::class)->findAll();
         return $this->render('shoe/index.html.twig', [
             'idShoe' => $id,
+            'shoes' => $shoes
         ]);
     }
 }
